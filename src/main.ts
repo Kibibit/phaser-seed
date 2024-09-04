5e80210c73cda923cf6f403ec46d471968c14f49
Neil Kalman <neil.kalman@growthspace.com>
2024-09-04 15:50:33 +0300

    add query param to change scenes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import Phaser from 'phaser';
 
 import KibibitLogoScene from './scenes/kibibit-logo.scene';
+import ProfileScene from './scenes/profile.scene';
+
+// select scene based on url params
+const urlParams = new URLSearchParams(window.location.search);
+const scene = urlParams.get('scene');
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
@@ -10,10 +15,13 @@ const config: Phaser.Types.Core.GameConfig = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 }
+      gravity: { y: 500 }
     }
   },
-  scene: [ KibibitLogoScene ]
+  // scene: [ KibibitLogoScene ]
+  scene: [
+    scene === 'profile' ? ProfileScene : KibibitLogoScene
+  ]
 };
 
 export default new Phaser.Game(config);
